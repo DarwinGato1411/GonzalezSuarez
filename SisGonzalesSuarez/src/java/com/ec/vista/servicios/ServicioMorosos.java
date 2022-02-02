f053024e1a8e9fff9e1c38cc73891d9680d89621
root <darwinvinicio14_11@gmail.com>
2022-02-01 20:35:42 -0500

    cambio lista morosos

---
+++ SisGonzalesSuarez/src/java/com/ec/vista/servicios/ServicioMorosos.java
@@ -0,0 +1,48 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ec.vista.servicios;
+
+import com.ec.servicio.HelperPersistencia;
+import com.ec.vistas.SriCatastro;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author gato
+ */
+public class ServicioMorosos {
+
+    private EntityManager em;
+
+    public EntityManager getEm() {
+        return em;
+    }
+
+    public void setEm(EntityManager em) {
+        this.em = em;
+    }
+
+    public List<SriCatastro> findMorosos(String valor) {
+
+        List<SriCatastro> listaSriCatastros = new ArrayList<SriCatastro>();
+        try {
+            em = HelperPersistencia.getEMF();
+            em.getTransaction().begin();
+            Query query = em.createQuery("SELECT a FROM PropietariosMorosos a ORDER BY a.propApellido ASC");      
+            listaSriCatastros = (List<SriCatastro>) query.getResultList();
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            System.out.println("Error en las consulta SriCatastro " + e.getMessage());
+        } finally {
+            em.close();
+        }
+
+        return listaSriCatastros;
+    }
+
+}
---
+++ SisGonzalesSuarez/src/java/com/ec/vistas/PropietariosMorosos.java
@@ -0,0 +1,111 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ec.vistas;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Darwin
+ */
+@Entity
+@Table(name = "propietarios_morosos")
+public class PropietariosMorosos implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Column(name = "id_medidor")
+    private Integer idMedidor;
+    @Size(max = 2147483647)
+    @Column(name = "porp_cedula")
+    private String porpCedula;
+    @Size(max = 2147483647)
+    @Column(name = "prop_nombre")
+    private String propNombre;
+    @Size(max = 2147483647)
+    @Column(name = "prop_apellido")
+    private String propApellido;
+    @Size(max = 2147483647)
+    @Column(name = "med_numero")
+    private String medNumero;
+    @Column(name = "fecha_ultimo_pago")
+    @Temporal(TemporalType.DATE)
+    private Date fechaUltimoPago;
+    // @Max(value=?)  @Min(value=?)//if you know range of your decimal fields consider using these annotations to enforce field validation
+    @Column(name = "dias_mora")
+    private Double diasMora;
+
+    public PropietariosMorosos() {
+    }
+
+    public Integer getIdMedidor() {
+        return idMedidor;
+    }
+
+    public void setIdMedidor(Integer idMedidor) {
+        this.idMedidor = idMedidor;
+    }
+
+    public String getPorpCedula() {
+        return porpCedula;
+    }
+
+    public void setPorpCedula(String porpCedula) {
+        this.porpCedula = porpCedula;
+    }
+
+    public String getPropNombre() {
+        return propNombre;
+    }
+
+    public void setPropNombre(String propNombre) {
+        this.propNombre = propNombre;
+    }
+
+    public String getPropApellido() {
+        return propApellido;
+    }
+
+    public void setPropApellido(String propApellido) {
+        this.propApellido = propApellido;
+    }
+
+    public String getMedNumero() {
+        return medNumero;
+    }
+
+    public void setMedNumero(String medNumero) {
+        this.medNumero = medNumero;
+    }
+
+    public Date getFechaUltimoPago() {
+        return fechaUltimoPago;
+    }
+
+    public void setFechaUltimoPago(Date fechaUltimoPago) {
+        this.fechaUltimoPago = fechaUltimoPago;
+    }
+
+    public Double getDiasMora() {
+        return diasMora;
+    }
+
+    public void setDiasMora(Double diasMora) {
+        this.diasMora = diasMora;
+    }
+    
+}

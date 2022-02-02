39195b8ebfe43e0d2e79821b499d9e40d229574a
root <darwinvinicio14_11@gmail.com>
2022-02-01 21:01:34 -0500

    cambios reportes

--- SisGonzalesSuarez/src/java/com/ec/vista/servicios/ServicioMorosos.java
+++ SisGonzalesSuarez/src/java/com/ec/vista/servicios/ServicioMorosos.java
@@ -5,6 +5,7 @@
 package com.ec.vista.servicios;
 
 import com.ec.servicio.HelperPersistencia;
+import com.ec.vistas.PropietariosMorosos;
 import com.ec.vistas.SriCatastro;
 import java.util.ArrayList;
 import java.util.List;
@@ -27,14 +28,15 @@ public class ServicioMorosos {
         this.em = em;
     }
 
-    public List<SriCatastro> findMorosos(String valor) {
+    public List<PropietariosMorosos> findMorosos(String propNombre) {
 
-        List<SriCatastro> listaSriCatastros = new ArrayList<SriCatastro>();
+        List<PropietariosMorosos> listado = new ArrayList<PropietariosMorosos>();
         try {
             em = HelperPersistencia.getEMF();
             em.getTransaction().begin();
-            Query query = em.createQuery("SELECT a FROM PropietariosMorosos a ORDER BY a.propApellido ASC");      
-            listaSriCatastros = (List<SriCatastro>) query.getResultList();
+            Query query = em.createQuery("SELECT a FROM PropietariosMorosos a WHERE a.propNombre LIKE :propNombre ORDER BY a.propApellido ASC");
+            query.setParameter("propNombre", "%" + propNombre + "%");
+            listado = (List<PropietariosMorosos>) query.getResultList();
             em.getTransaction().commit();
         } catch (Exception e) {
             System.out.println("Error en las consulta SriCatastro " + e.getMessage());
@@ -42,7 +44,7 @@ public class ServicioMorosos {
             em.close();
         }
 
-        return listaSriCatastros;
+        return listado;
     }
 
 }
